2b92137d11e682f8ef737ca40cbc54f37ca6c722
amazingvince <amazingvine@gmail.com>
2025-07-25 11:30:45 -0400

    Adding support for most pytorch optimizers to llmfoundry

--- llmfoundry/optim/__init__.py
+++ llmfoundry/optim/__init__.py
@@ -14,27 +14,58 @@ from llmfoundry.optim.no_op import NoOp
 from llmfoundry.optim.scheduler import InverseSquareRootWithWarmupScheduler
 from llmfoundry.registry import optimizers, schedulers
 
-optimizers.register('adalr_lion', func=DecoupledAdaLRLion)
-optimizers.register('clip_lion', func=DecoupledClipLion)
-optimizers.register('decoupled_lionw', func=DecoupledLionW)
-optimizers.register('decoupled_adamw', func=DecoupledAdamW)
-optimizers.register('no_op', func=NoOp)
+import torch
 
-schedulers.register('constant_with_warmup', func=ConstantWithWarmupScheduler)
+
+def register_pytorch_optimizers():
+    """Register all PyTorch optimizers with the LLM Foundry registry."""
+
+    # List of optimizers to skip
+    skip_optimizers = {
+        "Optimizer",  # Base class
+        "LBFGS",  # Requires closure, might need special handling
+    }
+
+    for name, obj in torch.optim.__dict__.items():
+        if (
+            isinstance(obj, type)
+            and issubclass(obj, torch.optim.Optimizer)
+            and name not in skip_optimizers
+        ):
+            registry_name = f"pytorch_{name.lower()}"
+            if registry_name not in optimizers._registry:
+                obj.__doc__ = (
+                    f"PyTorch {name} optimizer.\n\n"
+                    + (obj.__doc__ or "")
+                    + f"\n\nRegistered as '{registry_name}' in LLM Foundry."
+                )
+                optimizers.register(registry_name, func=obj)
+
+
+optimizers.register("adalr_lion", func=DecoupledAdaLRLion)
+optimizers.register("clip_lion", func=DecoupledClipLion)
+optimizers.register("decoupled_lionw", func=DecoupledLionW)
+optimizers.register("decoupled_adamw", func=DecoupledAdamW)
+optimizers.register("no_op", func=NoOp)
+
+# Register PyTorch optimizers
+register_pytorch_optimizers()
+
+schedulers.register("constant_with_warmup", func=ConstantWithWarmupScheduler)
 schedulers.register(
-    'cosine_with_warmup',
+    "cosine_with_warmup",
     func=CosineAnnealingWithWarmupScheduler,
 )
-schedulers.register('linear_decay_with_warmup', func=LinearWithWarmupScheduler)
+schedulers.register("linear_decay_with_warmup", func=LinearWithWarmupScheduler)
 schedulers.register(
-    'inv_sqrt_with_warmup',
+    "inv_sqrt_with_warmup",
     func=InverseSquareRootWithWarmupScheduler,
 )
 
 __all__ = [
-    'DecoupledLionW',
-    'DecoupledClipLion',
-    'DecoupledAdaLRLion',
-    'NoOp',
-    'InverseSquareRootWithWarmupScheduler',
+    "DecoupledLionW",
+    "DecoupledClipLion",
+    "DecoupledAdaLRLion",
+    "NoOp",
+    "InverseSquareRootWithWarmupScheduler",
 ]
